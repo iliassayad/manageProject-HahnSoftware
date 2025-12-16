add1309811334d7228c25b08265e56a307635a34
AYAD Iliass <ayadilyass282@gmail.com>
2025-12-16 12:04:46 +0100

    first commit

---
+++ manageProject-backend/src/main/java/net/ayad/manageprojectbackend/ManageProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package net.ayad.manageprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageProjectBackendApplication.class, args);
+    }
+
+}
